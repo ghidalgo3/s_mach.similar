21d6ad4751d9660c4b8477a58237276e63f58384
Lance Gatlin <lance.gatlin@gmail.com>
2014-10-06 15:01:11 -0400

    more placeholders

--- src/main/scala/s_mach/similar/Levenshtein.scala
+++
@@ -1,8 +0,0 @@
-package s_mach.similar
-
-// http://en.wikipedia.org/wiki/Levenshtein_distance
-object LevenshteinDistance {
-  implicit object CanSimilar_String_String extends CanSimilar[String] {
-    override def similar(t1: String, t2: String): Double = ???
-  }
-}
--- src/main/scala/s_mach/similar/SimilarOps.scala
+++ src/main/scala/s_mach/similar/SimilarOps.scala
@@ -5,4 +5,15 @@ object SimilarOps {
   def calcJaccardIndex(a_intersect_b_size: Int, a_size: Int, b_size: Int) : Double = ???
   // http://en.wikipedia.org/wiki/S%C3%B8rensen%E2%80%93Dice_coefficient
   def calcDiceCoefficient(a_intersect_b_size: Int, a_size: Int, b_size: Int) : Double = ???
+
+  // http://en.wikipedia.org/wiki/Levenshtein_distance
+  def levenshteinDistance(s1: String, s2: String) : Int = ???
+
+  // http://en.wikipedia.org/wiki/Hamming_distance
+  def hammingDistance(s1: String, s2: String) : Int = ???
+
+  // http://en.wikipedia.org/wiki/Needleman%E2%80%93Wunsch_algorithm
+  def needlemanWunsch(s1: String, s2: String) : Int = ???
+
+  // Implement more algos here: http://web.archive.org/web/20081224234350/http://www.dcs.shef.ac.uk/~sam/stringmetrics.html
 }
